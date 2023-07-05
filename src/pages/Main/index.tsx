9e8cf12c74b81351696aabbccf0acf17b34709dc
Danylo Dovban <epumbar@gmail.com>
2023-07-05 14:23:58 +0300

    +Modal window +Init screen media query

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,20 +1,28 @@
 import React, { FC, ReactElement, useState } from "react";
 import "./index.style.css";
+import Modal from "./main.modal";
 
 const Main: FC<any> = (): ReactElement => {
   const [count, setCount] = useState("25");
+  const [modal, setModal] = useState(false);
+
+  const handleClick = () => {
+    setModal(true);
+  };
+
   return (
     <div className="main">
       <div className="ai">
         <div className="emoji">🤖</div>
       </div>
-      <div className="central-block">
+      <div className="central-block" onClick={handleClick}>
         <p className="counter">{count}</p>
         <div className="emoji-spec">🔥</div>
       </div>
       <div className="player">
         <div className="emoji">👨🏻</div>
       </div>
+      {modal ? <Modal matches={count} /> : null}
     </div>
   );
 };
